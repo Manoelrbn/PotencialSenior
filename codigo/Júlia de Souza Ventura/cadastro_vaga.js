3e026ce0ec605c107d40bd55e980ca1daf3d90ee
Julia Ventura <68651976+juliaszventura@users.noreply.github.com>
2024-11-03 23:35:02 -0300

    Add files via upload

---
+++ codigo/Júlia de Souza Ventura/cadastro_vaga.js
@@ -0,0 +1,18 @@
+function submitForm() {
+    
+    const cadastroVagaForm = {
+        nome_empresa: document.getElementById("nome-empresa").value,
+        cnpj: document.getElementById("cnpj").value,
+        categoria_empresa: document.getElementById("categoria-empresa").value,
+        nome_vaga: document.getElementById("nome-vaga").value,
+        salario: document.getElementById("salario").value,
+        localizacao: document.getElementById("localizacao").value,
+        resumo: document.getElementById("resumo").value,
+        responsabilidades: document.getElementById("responsabilidades").value,
+        requisitos: document.getElementById("requisitos").value
+    };
+
+    sessionStorage.setItem("cadastroVagaForm", JSON.stringify(cadastroVagaForm));
+
+    window.location.href = "exibir_vaga.html";
+}
